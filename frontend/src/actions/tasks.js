10529be2249c4a07682e98af3547838b3337fa5f
jobbins <j.reboss@gmail.com>
2019-04-14 13:29:12 -0400

    Final touches

--- frontend/src/actions/tasks.js
+++ frontend/src/actions/tasks.js
@@ -1,7 +1,12 @@
 import * as actions from './actionManifest'
 import axios from 'axios'
 
-const url = process.env.PRODUCTION_URL || 'http://localhost:3001';
+// When running in dev mode, the api will run on port 3001
+let url = 'http://localhost:3001';
+if (process.env.NODE_ENV === 'production')
+	// when in production, api will run on port 3000 and serve the built index.html
+	// alternatively, one can specify the production url if splitting the project into microservices, etc.
+	url = process.env.PRODUCTION_URL || 'http://localhost:3000';
 
 // Get tasks
 export function getTasks() {
